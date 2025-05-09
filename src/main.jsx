e1b64ca14f69d7352e392f91980e5a9147e8298e
Bob Vernam <Bob62683@yahoo.com>
2025-05-09 10:07:59 -0400

    Add full React project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </Router>
 );
